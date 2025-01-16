4ef30caf525d6d86a9456912133b5baa80a7f3a0
Abdellah <aitoubihia@gmail.com>
2025-01-16 21:02:17 +0100

    churn_rate

--- home_exchange/models/interm/monthy_sub.sql
+++ home_exchange/models/interm/monthy_sub.sql
@@ -10,7 +10,10 @@
 SELECT
 
 FORMAT_TIMESTAMP('%Y-%m', subscription_date) AS sub_month,
+count(user_id) as user_count,
+sum(renew) as renew_count,
 
-*
  FROM {{ ref('stg_subscriptions') }}
  WHERE subscription_date between '2019-01-01' and '2021-12-31'
+ group by sub_month
+ order by sub_month asc
--- home_exchange/models/staging/stg_exchanges.sql
+++ home_exchange/models/staging/stg_exchanges.sql
@@ -11,7 +11,6 @@ WITH cleaned_exchanges AS (
         region AS ex_region,
         department AS ex_department,
         city AS ex_city,
-        -- -- Preserve othe
 
         -- -- Preserve other columns
         * EXCEPT(created_at, start_on, end_on, guest_countguest_count, country, region, department, city)
@@ -19,3 +18,17 @@ WITH cleaned_exchanges AS (
     FROM {{  ref('base_bigquery_exchanges') }}
 )
 SELECT * FROM cleaned_exchanges
+WHERE NOT (
+    -- Condition 1: Exclude rows where 'canceled_at' is null but 'user_cancellation_id' has a value
+    (canceled_at IS NOT NULL AND user_cancellation_id IS NULL)
+
+    OR
+
+    -- Condition 2: Exclude rows where 'finalized_at' is null but 'canceled_at' has a value
+    (finalized_at IS NOT NULL AND canceled_at IS NULL)
+
+    OR
+
+    -- Condition 3: Exclude rows where 'start_on' date is greater than or equal to 'end_on' date
+    (start_on >= end_on)
+)
